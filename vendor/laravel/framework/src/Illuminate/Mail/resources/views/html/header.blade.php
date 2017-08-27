dcf542df10906f9ad350d0e40bb34a54f0ff8f25
Sam W <swilliams96@outlook.com>
2017-08-27 21:03:36 +0100

    Adding vendor/laravel to include changes to core templates (emails etc)

---
+++ vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/header.blade.php
@@ -0,0 +1,8 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}">
+            <!--{{ $slot }}-->
+            <span class="stu">STU</span><span class="pad">PAD</span>
+        </a>
+    </td>
+</tr>
