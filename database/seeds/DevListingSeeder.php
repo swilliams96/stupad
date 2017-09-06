6691d02be61798e10757490154d636d533927344
Sam W <swilliams96@outlook.com>
2017-09-06 19:14:00 +0100

    Fixed issue with seeders and implemented Carbon

--- database/seeds/DevListingSeeder.php
+++ database/seeds/DevListingSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class DevListingSeeder extends Seeder
 {
@@ -17,10 +18,10 @@ class DevListingSeeder extends Seeder
         DB::statement('SET FOREIGN_KEY_CHECKS=1');
 
         // Insert listings data
-        DB::table('locations')->insert([
+        DB::table('listings')->insert([
             [
-                'active_datetime' => date('Y-m-d H:i:s'),
-                'inactive_datetime' => strtotime('+7 days', date('Y-m-d H:i:s')),
+                'active_datetime' => Carbon::now(),
+                'inactive_datetime' => Carbon::now()->addDays(14),
                 'title' => 'Sydney Place',
                 'landlord_id' => 2,
                 'rent_value' => 110,
@@ -28,7 +29,6 @@ class DevListingSeeder extends Seeder
                 'description' => 'First floor apartment featuring neutral décor and floor coverings. The furnished accommodation includes a spacious sitting room with period fireplace and full height sash windows, kitchen with granite work tops, gas hob/cooker/extractor, fridge/freezer and washing machine, spacious double bedroom with built in storage and recently fitted shower room with waterfall shower. Further benefits such as gas central heating and residents permit parking. Conveniently located within a level walk of the city centre. Would ideally suit a single professional applicant or couple, with post graduates and mature students considered.',
                 'short_description' => 'First floor apartment featuring neutral décor and floor coverings. The furnished accommodation includes a spacious sitting room with period fireplace and full height sash windows',
                 'area_id' => 1,
-                'locations' => '1,2,3,4,5,6',
                 'lat' => 51.386156062079,
                 'lng' => -2.35189667259341,
                 'bedrooms' => 0,
@@ -37,8 +37,8 @@ class DevListingSeeder extends Seeder
                 'furnished' => true,
                 'bills_included' => true,
                 'pets_allowed' => true,
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ],
         ]);
     }
