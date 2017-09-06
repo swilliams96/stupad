b5757ea1b2e3dfaf8d4b97d4620055dae709e361
Sam W <swilliams96@outlook.com>
2017-09-06 19:18:04 +0100

    Implemented Carbon into DevLocationSeeder too

--- database/seeds/DevLocationSeeder.php
+++ database/seeds/DevLocationSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class DevLocationSeeder extends Seeder
 {
@@ -18,12 +19,12 @@ class DevLocationSeeder extends Seeder
 
         // Insert the locations data
         DB::table('locations')->insert([
-            ['name' => 'Bath Town', 'short_name' => 'Bath', 'slug' => 'bath-town', 'area_id' => 1, 'active' => true, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['name' => 'University of Bath', 'short_name' => 'Uni of Bath', 'slug' => 'bath', 'area_id' => 1, 'active' => true, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['name' => 'Bath Spa University', 'short_name' => 'Bath Spa', 'slug' => 'bathspa', 'area_id' => 1, 'active' => true, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['name' => 'University of Bristol', 'short_name' => 'Uni of Bristol', 'slug' => 'bristol', 'area_id' => 2, 'active' => true, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['name' => 'University of West England', 'short_name' => 'UWE', 'slug' => 'uwe', 'area_id' => 2, 'active' => true, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
-            ['name' => 'Bristol Town', 'short_name' => 'Bristol', 'slug' => 'bristol-town', 'area_id' => 2, 'active' => true, 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+            ['name' => 'Bath Town', 'short_name' => 'Bath', 'slug' => 'bath-town', 'area_id' => 1, 'active' => true, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'University of Bath', 'short_name' => 'Uni of Bath', 'slug' => 'bath', 'area_id' => 1, 'active' => true, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'Bath Spa University', 'short_name' => 'Bath Spa', 'slug' => 'bathspa', 'area_id' => 1, 'active' => true, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'University of Bristol', 'short_name' => 'Uni of Bristol', 'slug' => 'bristol', 'area_id' => 2, 'active' => true, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'University of West England', 'short_name' => 'UWE', 'slug' => 'uwe', 'area_id' => 2, 'active' => true, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'Bristol Town', 'short_name' => 'Bristol', 'slug' => 'bristol-town', 'area_id' => 2, 'active' => true, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
         ]);
     }
 }
