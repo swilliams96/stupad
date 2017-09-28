09fe1dda81c8205256fc888b14f79085d8904f77
Sam W <swilliams96@outlook.com>
2017-09-28 03:17:11 +0100

    Updated layout of info buttons on listing page

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -14,33 +14,43 @@
                 <div class="col-lg-8 col-lg-offset-2 col-md-12">
                     <a class="listing-back-link" href="{{ Cookie::has('lastsearch_location') ? (route('results') . '/' . App\University::where('name', Cookie::get('lastsearch_location'))->first()->slug) : route('search') }}"><i class="fa fa-chevron-left fa-pad-5"></i>Back to search ...</a>
 
-                    <!-- TODO : rewrite this using bootstrap to automatically stack vertically on mobile -->
                     <div class="listing-page-buttons">
                         <!-- Below save button will need to be a hidden form with a POST link to a Favourites Controller in Laravel --></share-button>
-                        <a href="javascript:;" class="btn btn-save-listing btn-xl">Share<i class="fa fa-share-alt fa-pad-5l"></i></a>
+                        <a href="javascript: return false;" class="btn btn-save-listing btn-xl">Share<i class="fa fa-share-alt fa-pad-5l"></i></a>
                         <a href="javascript:;" class="btn btn-save-listing btn-xl">Save<i class="fa fa-heart fa-pad-5l"></i></a>
                         <a href="#" class="btn btn-primary btn-xl">Contact<i class="fa fa-user-circle fa-pad-5l"></i></a>
                     </div>
 
-
-
                     <div class="listing-page-description">
                         <h2>Description</h2>
                         @foreach ($description as $paragraph)<p>{{ $paragraph }}</p>@endforeach
-                        <ul class="listing-icons">
-                            <li title="{{ $listing->bedrooms == 0 ? 'Studio (1 bedroom)' : ($listing->bedrooms . ($listing->bedrooms > 1 ? ' bedrooms' : ' bedroom')) }}"><i class="fa fa-bed fa-pad-5"></i>{{ $listing->bedrooms == 0 ? 'Studio' : $listing->bedrooms }}</li>
-                            <li title="{{ $listing->bathrooms . ($listing->bathrooms > 1 ? ' bathrooms' : ' bathroom') }}"><i class="fa fa-bath fa-pad-5"></i>{{ $listing->bathrooms }}</li>
-                            @if ($listing->town_distance != null)<li title="{{ $listing->town_distance . ($listing->town_distance > 1 ? ' mins' : ' min') . ' from ' . $listing->area->name . ' centre' }}"><i class="fa fa-map-marker fa-pad-5"></i>{{ $listing->town_distance . ($listing->town_distance > 1 ? ' mins' : ' min') }}</li>@endif
-                            @if ($listing->furnished == true)<li title="Furnished"><i class="fa fa-check fa-pad-5"></i>Furnished</li>@endif
-                            @if ($listing->bills_included == true)<li title="Bills included"><i class="fa fa-envelope-open fa-pad-5"></i>Bills Included</li>@endif
-                            @if ($listing->pets_allowed == true)<li title="Pets allowed"><i class="fa fa-paw fa-pad-5"></i>Allowed</li>@endif
-                        </ul>
                     </div>
+
+                    <h2>Information</h2>
+                    <ul class="listing-page-info">
+                        <li><i class="fa fa-bed"></i>{{ $listing->bedrooms == 0 ? 'Studio (1 bedroom)' : ($listing->bedrooms . ($listing->bedrooms > 1 ? ' bedrooms' : ' bedroom')) }}</li>
+                        <li><i class="fa fa-bath"></i>{{ $listing->bathrooms . ($listing->bathrooms > 1 ? ' bathrooms' : ' bathroom') }}</li>
+                        @if ($listing->town_distance != null)<li><i class="fa fa-map-marker"></i>{{ $listing->town_distance . ($listing->town_distance > 1 ? ' mins' : ' min') . ' from ' . $listing->area->name . ' ' . $listing->area->suffix . ' centre' }}</li>@endif
+                        @if ($listing->furnished == true)<li><i class="fa fa-check"></i>Furnished</li>@endif
+                        @if ($listing->bills_included == true)<li><i class="fa fa-envelope-open"></i>Bills included</li>@endif
+                        @if ($listing->pets_allowed == true)<li><i class="fa fa-paw"></i>Pets allowed</li>@endif
+                    </ul>
+
+                    <!--
+                    <ul class="listing-icons">
+                        <li title="{{ $listing->bedrooms == 0 ? 'Studio (1 bedroom)' : ($listing->bedrooms . ($listing->bedrooms > 1 ? ' bedrooms' : ' bedroom')) }}"><i class="fa fa-bed fa-pad-5"></i>{{ $listing->bedrooms == 0 ? 'Studio' : $listing->bedrooms }}</li>
+                        <li title="{{ $listing->bathrooms . ($listing->bathrooms > 1 ? ' bathrooms' : ' bathroom') }}"><i class="fa fa-bath fa-pad-5"></i>{{ $listing->bathrooms }}</li>
+                        @if ($listing->town_distance != null)<li title="{{ $listing->town_distance . ($listing->town_distance > 1 ? ' mins' : ' min') . ' from ' . $listing->area->name . ' centre' }}"><i class="fa fa-map-marker fa-pad-5"></i>{{ $listing->town_distance . ($listing->town_distance > 1 ? ' mins' : ' min') }}</li>@endif
+                        @if ($listing->furnished == true)<li title="Furnished"><i class="fa fa-check fa-pad-5"></i>Furnished</li>@endif
+                        @if ($listing->bills_included == true)<li title="Bills included"><i class="fa fa-envelope-open fa-pad-5"></i>Bills Included</li>@endif
+                        @if ($listing->pets_allowed == true)<li title="Pets allowed"><i class="fa fa-paw fa-pad-5"></i>Allowed</li>@endif
+                    </ul>
+                    -->
 
                     <div class="listing-page-gallery">
                         <h2>Gallery</h2>
                         <!-- TODO: LISTING IMAGES FROM LISTING IMAGE TABLE -->
-                        <a href="{{ $listing->header->file() }}" data-caption="" data-fancybox="gallery" data-type="image"draggable=false >
+                        <a href="{{ $listing->header->file() }}" data-caption="" data-fancybox="gallery" data-type="image" draggable=false >
                             <img src="{{ $listing->header->file() }}" draggable=false />
                         </a>
                         <div class="additional-images">
